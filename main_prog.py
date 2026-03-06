0130e7c6d46fb54734dc29c1a14496e0eb6b95ad
YJChen <yj.chen.working@gmail.com>
2026-03-07 00:07:09 +0800

    add file

--- main_prog.py
+++ main_prog.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import subprocess
 import os
-
+## Movement, move to, move along, vector_motion, .....degree motion...etc
 class RoboCupTacticalBoard:
     def __init__(self, root):
         self.root = root
